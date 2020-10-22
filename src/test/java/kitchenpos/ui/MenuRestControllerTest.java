d1987891a90789c43eec8fa0e3e11ee46735bb1e
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-10-22 22:33:19 +0900

    test : MenuRestControllerTest.java 추가

--- src/test/java/kitchenpos/ui/MenuRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuRestControllerTest.java
@@ -1,15 +1,24 @@
 package kitchenpos.ui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kitchenpos.application.MenuService;
+import kitchenpos.domain.Menu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringRunner.class)
 @WebMvcTest(MenuRestController.class)
 class MenuRestControllerTest {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Autowired
     private MockMvc mockMvc;
@@ -25,17 +35,27 @@ class MenuRestControllerTest {
     @MockBean
     private MenuService menuService;
 
-    @Test
-    void create() throws Exception {
-        mockMvc.perform(post("/api/menus"))
-                .andDo(print())
-                .andExpect(status().isOk());
+    @BeforeEach
+    void setup(){
     }
 
+    @DisplayName("메뉴를 추가한다.")
     @Test
-    void list() throws Exception {
-        mockMvc.perform(get("/api/menus"))
+    void create() throws Exception {
+        Menu request = new Menu();
+        request.setId(0L);
+        request.setName("후라이드 치킨");
+        request.setPrice(new BigDecimal(19000));
+        request.setMenuGroupId(0L);
+        request.setMenuProducts(new ArrayList<>());
+        when(menuService.create(any(Menu.class))).thenReturn(request);
+
+        String requestAsString = OBJECT_MAPPER.writeValueAsString(request);
+
+        this.mockMvc.perform(post("/api/menus/")
+                .content(requestAsString).contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isCreated());
     }
+
 }
